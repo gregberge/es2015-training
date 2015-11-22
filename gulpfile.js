d46f6b7cee9029bbcf17198a5f313e677171a9d0
Greg Bergé <neoziro@gmail.com>
2015-11-22 20:54:44 +1100

    Exercise 12 : set up babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var babel = require('gulp-babel');
 
 gulp.task('browserSync', function () {
   browserSync.init({
@@ -9,4 +10,14 @@ gulp.task('browserSync', function () {
   })
 });
 
-gulp.task('default', ['browserSync']);
+gulp.task('babel', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', ['babel'], function () {
+  return gulp.watch('src/**/*.js', ['babel']);
+});
+
+gulp.task('default', ['browserSync', 'watch']);
