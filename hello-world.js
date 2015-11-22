411f60578955f76655bfd7c4290a96a591f09521
Greg Bergé <neoziro@gmail.com>
2015-11-22 15:47:59 +1100

    Exercise 4 : Hello prototype

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,30 @@
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-
-function getEllapsedMinutes() {
-  return hours * 60 + minutes;
+function Hello(name) {
+  this.name = name;
 }
 
-console.log('hello world! ' + hours + ':' + minutes + ' (' + getEllapsedMinutes() + ' min)');
+Hello.prototype.say = function () {
+  return 'hello ' + this.name + '!';
+};
+
+function HelloDate() {
+  Hello.apply(this, arguments);
+}
+
+HelloDate.prototype = Object.create(Hello.prototype);
+
+HelloDate.prototype.say = function () {
+  var hello = Hello.prototype.say.call(this);
+
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  function getEllapsedMinutes() {
+    return hours * 60 + minutes;
+  }
+
+  return hello + ' ' + hours + ':' + minutes + ' (' + getEllapsedMinutes() + ' min)';
+};
+
+var hello = new HelloDate('Johnny');
+console.log(hello.say());
