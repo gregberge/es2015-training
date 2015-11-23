519124858e7ac57e87e679cd48c2c54940eac56e
Greg Bergé <neoziro@gmail.com>
2015-11-23 20:35:12 +1100

    Exercise 16 : add submit message

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@ var rateForm = document.getElementById('rateForm');
 rateForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
+  var alert = rateForm.querySelector('.alert');
   var rate = +rateForm.rate.value;
-
-  if (Number.isInteger(rate) && rate >= 1 && rate <= 5)
-    alert('OK');
-  else
-    alert('NOK');
-
   var movie = rateForm.movie.value;
+  var valid = Number.isInteger(rate) && rate >= 1 && rate <= 5;
+
+  alert.style.display = 'block';
+
+  alert.classList.toggle('alert-danger', !valid);
+  alert.classList.toggle('alert-success', valid);
+
+  alert.innerHTML = valid
+    ? `The movie ${movie} has been rated ${rate}!`
+    : `Invalid rate ${rate}.`;
 
   if (movie.startsWith('007'))
     new Audio('http://downloadwap.com/mp3tones/rtones/new/tv-movie/james_bond_007_original-4820.mp3').play();
